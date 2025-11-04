9a5b0e2302fcf025305d6688487440ba5ae44d62
Raymond Shao <rshao@ucla.edu>
2025-11-03 16:42:07 -0800

    script to scrape all magazine articles (no digital exclusive)

--- atlantic.py
+++ atlantic.py
@@ -6,10 +6,6 @@ import time
 def record_year(driver, workbook, year):
     HEADER = ['Headline', 'Link', 'Date', 'Summary']
     BOLD = workbook.add_format({'bold':True})
-    WRAP = workbook.add_format()
-    WRAP.set_shrink()
-    WRAP.set_font_name('Calibri')
-    WRAP.set_font_size(12)
 
     worksheet = workbook.add_worksheet(str(year))
     worksheet.write_row(0, 0, HEADER, BOLD)
@@ -20,29 +16,30 @@ def record_year(driver, workbook, year):
     for month in months:
         driver.get(f"https://www.theatlantic.com/magazine/toc/{year}/{month}/")
         elements = driver.find_elements(By.XPATH, "//a[contains(@class,'GridItem_hedLink')]")
-
+        articles = {}
         for element in elements:
-            link = element.get_attribute("href")
-            isFriend, abstract = check_article(driver, link)
-            if not isFriend:
-                continue
-            row_to_write = (element.text, element.get_attribute("href"), str(year) + "-" + month, abstract)
-            worksheet.write_row(cur_row, 0, row_to_write, WRAP)
-            cur_row += 1
+            articles[element.get_attribute("href")] = (element.text, element.get_attribute("href"), str(year) + "-" + month)
+        cur_row = check_articles(driver, worksheet, cur_row, articles)
 
-def check_article(driver, link):
-    driver.get(link)
-    if len(driver.find_elements(By.XPATH, "//*[contains(text(), 'friend')]")) == 0:
-        return (False, None)
+def check_articles(driver, worksheet, cur_row, articles):
+    WRAP = workbook.add_format()
+    WRAP.set_shrink()
+    WRAP.set_font_name('Calibri')
+    WRAP.set_font_size(12)
+    for link in articles:
+        driver.get(link)
+        if len(driver.find_elements(By.XPATH, "//*[contains(text(), 'friend')]")) == 0:
+            continue
+        abstract = ''
+        try:
+            element = driver.find_element(By.XPATH, "//p[contains(@class,'ArticleDek_feature')]")
+            abstract = element.text
+        except:
+            pass
+        worksheet.write_row(cur_row, 0, articles[link]+(abstract,), WRAP)
+        cur_row += 1
 
-    abstract = ''
-    try:
-        abstract = driver.find_element(By.XPATH, "//p[contains(@class,'ArticleDek_feature')]")
-    except:
-        continue
-    driver.back()
-
-    return (True, abstract)
+    return cur_row
 
 def sign_in(driver):
     email = "jkrems@gmail.com"
